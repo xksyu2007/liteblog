0ac11d84063a08cb3426e601d4d4023d2900e043
夏空拾雨 <acc@xksyu.cn>
2026-08-26 13:36:52 +0800

    page.feat tool page

--- page/src/pages/Tool.vue
+++ page/src/pages/Tool.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
-import type {article} from "@misc/interface.ts";
+import type {tool} from "@misc/interface.ts";
 
-const config = ref<article>()
+const config = ref<tool>()
 onMounted(async () => {
     const response1 = await fetch('/config/tool.json')
     config.value = await response1.json()
@@ -16,7 +16,7 @@ onMounted(async () => {
                 <router-link class="head-back" to="/">
                     <i class="bi bi-arrow-left-circle size-big-title"></i>
                 </router-link>
-                <div class="size-big-title">Misc Article</div>
+                <div class="size-big-title">Tool</div>
             </div>
 
             <div v-if="!config" class="load">
@@ -24,6 +24,17 @@ onMounted(async () => {
                 Loading ...
             </div>
 
+            <div v-if="config" class="tool-layout no-select">
+                <div v-for="item in config" class="tool-single">
+                    <div class="tool-single-title size-title"
+                         @click="$router.push(`/tool/${encodeURIComponent(item.file)}`)">
+                        {{item.name}}
+                    </div>
+                    <div class="tool-single-content size-content">
+                        {{item.abstract}}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -62,5 +73,57 @@ onMounted(async () => {
     flex-direction: column;
     align-items: center;
     justify-content: center;
+}
+
+.tool-layout{
+    columns: 3;
+    column-gap: 20px;
+    width: 100%;
+    margin-top: 20px;
+}
+
+.tool-layout > :deep(*) {
+    margin-top: 0;
+}
+
+.tool-single{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    break-inside: avoid;
+    text-wrap: pretty;
+    white-space: pre-wrap;
+    margin-bottom: 20px;
+}
+
+.tool-single-title{
+    padding: 20px;
+    border-radius: 25px;
+    text-wrap: balance;
+}
+
+.tool-single:nth-child(even) .tool-single-title{
+    background-color: aliceblue;
+}
+.tool-single:nth-child(odd) .tool-single-title{
+    background-color: floralwhite;
+}
+
+@media screen and (max-width: 768px) {
+    .head-back{
+        width: 20%;
+    }
+
+    .tool-layout {
+        columns: 1;
+        column-gap: 15px;
+    }
+}
+
+@media screen and (min-width: 769px) and (max-width: 1280px) {
+    .tool-layout {
+        columns: 2;
+        column-gap: 15px;
+    }
 }
 </style>
