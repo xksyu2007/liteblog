a5f90428cb32b8c7165ced32ebffe9a148e86b8f
夏空拾雨 <acc@xksyu.cn>
2026-08-26 14:08:17 +0800

    page.feat tool page

--- page/src/pages/inner/ToolView.vue
+++ page/src/pages/inner/ToolView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+import {defineAsyncComponent, shallowRef, watch} from "vue";
+import "@pages/inner/override.css"
 
+const route = useRoute()
+const Page = shallowRef();
+watch(
+    () => route.params.id,
+    (newId) => {
+        if (newId) {
+            const componentName = (newId as string).replace('.vue', '');
+            Page.value = defineAsyncComponent(() => import(`@tools/${componentName}.vue`));
+        }
+    },
+    { immediate: true }
+);
 </script>
 
 <template>
-
+    <Page/>
 </template>
 
 <style scoped>
